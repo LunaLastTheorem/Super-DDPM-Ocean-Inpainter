2db4fb2ab1278a07b8128ea4b2fa2fc94c888fa7
David <dluna777@outlook.com>
2025-07-21 09:53:54 -0700

    🪑added more chairs, and all of them are now the same size

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ const splash = [
     "Now with a killer playlist!"
 ]
 
-const Chair_image = "./src/chair.jpg"
+const Chair_images = [
+    "./src/chair0.jpg",
+    "./src/chair1.png",
+    "./src/chair2.jpg",
+    "./src/chair3.jpg",
+    "./src/chair4.jpg",
+    "./src/chair5.jpg"
+]
 
-window.onload = function() {
+window.onload = function () {
     const Splash_view = document.getElementById("Splash");
     Splash_view.innerHTML = Choose_splash();
 
@@ -20,28 +27,32 @@ window.onload = function() {
     Splash_box.classList.add("vibrate-1");
 }
 
-function Add_chair(){
+function Get_chair() {
+    return Chair_images[Math.floor(Math.random() * Chair_images.length)]
+}
+
+function Add_chair() {
     console.log("Appending chair");
-    
+
     const Chair_space = document.getElementById("Chair_space");
     const Chair_image_element = document.createElement("img")
 
-    Chair_image_element.src = Chair_image
+    Chair_image_element.src = Get_chair()
     Chair_image_element.classList.add("vibrate-1")
     Chair_space.appendChild(Chair_image_element)
 }
 
-function Choose_splash(){
+function Choose_splash() {
     return splash[Math.floor(Math.random() * splash.length)];
 }
 
-function Noise_function(){
+function Noise_function() {
     console.log("divergence free noise")
 }
 
 // main game loop
-setInterval(function(){
-    if(Math.random() < 0.1){
+setInterval(function () {
+    if (Math.random() < 0.05) {
         window.alert("DrCaley: Zoom?")
     }
 }, 1000);
